cf323a3345b13c974b1cd7f507a9829ff88571b1
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2024-06-05 17:00:48 +0400

    finish slider

---
+++ front/index.js
@@ -0,0 +1,71 @@
+// carousel
+const carouselImages = document.querySelector('.carousel-images');
+const images = document.querySelectorAll('.carousel-images img');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const indicators = document.querySelectorAll('.indicator');
+let currentIndex = 0;
+let interval;
+
+function updateCarousel() {
+    const width = images[currentIndex].clientWidth;
+    carouselImages.style.transform = `translateX(-${currentIndex * width}px)`;
+    indicators.forEach((indicator, index) => {
+        indicator.classList.toggle('active', index === currentIndex);
+    });
+}
+
+function showNextImage() {
+    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+    updateCarousel();
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(showNextImage, 3000);
+}
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+    updateCarousel();
+    resetInterval();
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+    updateCarousel();
+    resetInterval();
+});
+
+indicators.forEach(indicator => {
+    indicator.addEventListener('click', () => {
+        currentIndex = parseInt(indicator.getAttribute('data-index'));
+        updateCarousel();
+        resetInterval();
+    });
+});
+
+window.addEventListener('resize', updateCarousel);
+
+interval = setInterval(showNextImage, 3000);
+updateCarousel();
+// end carousel
+
+// services comment 
+function showText(id) {
+    
+    const spans = document.querySelectorAll('.comment-service span');
+    spans.forEach(span => span.style.display = 'none');
+    
+    
+    const spanToShow = document.getElementById(id);
+    if (spanToShow) {
+        spanToShow.style.display = 'block';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const spans = document.querySelectorAll('.comment-service span');
+    spans.forEach(span => span.style.display = 'none');
+});
